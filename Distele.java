e72ecb096563bb68f8ad4855f66040f84c57b7d0
Balvannanathan <113589227+balamt15@users.noreply.github.com>
2022-09-23 09:18:08 -0700

    Update Distele.java

--- Distele.java
+++ Distele.java
@@ -4,17 +4,17 @@ public static void main(String args[]){
 int arr[] ={10,30,40,50,20,10,40};
 int n = arr.length;
 
-int count=0;
-for(int i=0;i<n;i++){
-	for(int j=0;j<n;j++){
-		if(arr[i] == arr[j]){
+int count=1;
+for(int i=1;i<n;i++){
+	int j=0;
+	for(j=0;j<i;j++){
+		if(arr[i] == arr[j])
 			break;
-			}
-		if(i == j){
-			count++;
-			}
-		}	
+		}
+	if(i == j){
+		count++;
+		}
 	}
-
+	System.out.println(count);
 	}
 }
